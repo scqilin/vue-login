0be6384ee1c59facac16404b17c1a6e6c9cb15f3
scqilin <sql1211@qq.com>
2024-05-30 11:32:40 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css'
+
+createApp(App).use(Layui).mount('#app')
